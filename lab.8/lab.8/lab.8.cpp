1a4afd45b2a441c2221c3383392e2fee7ae517db
pitr0theAct <120792390+pitr0theAct@users.noreply.github.com>
2022-12-23 15:04:20 +0300

    Add files via upload

---
+++ lab.8/lab.8/lab.8.cpp
@@ -0,0 +1,23 @@
+﻿//Дано целое число типа unsigned int, выведите на экран содержимое каждого из его байтов, используя знания по указателям.
+
+#include <iostream>
+
+int main()
+{
+	unsigned int n = UINT_MAX;
+	int byte;
+	char* pb;
+	pb = (char*)&n;
+	for (char* i = pb; i < pb + 4; i++)
+	{
+		byte = (int)*i;
+		if (byte < 0)
+		{
+			std::cout << byte + 256 << " ";
+		}
+		else
+		{
+			std::cout << byte << " ";
+		}
+	}
+}
