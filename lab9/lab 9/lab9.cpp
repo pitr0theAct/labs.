c8d282dc0796443652b3b3022258d621f788f498
pitr0theAct <120792390+pitr0theAct@users.noreply.github.com>
2022-12-23 15:04:49 +0300

    Add files via upload

---
+++ lab9/lab 9/lab9.cpp
@@ -0,0 +1,37 @@
+﻿
+//Найти объем куба.
+
+#include <iostream>
+//Класс содержащий информацию о кубе
+class Cube
+{
+private:
+    unsigned int m_sides; //СторнЫ
+public:
+    //Принимает значения высоты и сторон 
+    Cube(unsigned int sides)
+    {
+        m_sides = sides;
+    }
+    //Возвращает значение объема 
+    unsigned int CubeArea()
+    {
+        return (m_sides*m_sides*m_sides);
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Введите стороны куба" << std::endl;
+    int K;//Принимает значение сторон куба
+    std::cin >> K;
+    if (K > 0)
+    {
+        std::cout << std::endl;
+        Cube Cube(K);
+        std::cout << "Объем куба:" << " ";
+        std::cout << Cube.CubeArea() << std::endl;
+    }
+}
+
