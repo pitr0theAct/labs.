508f86e2e8ee444d9b423dfe1362031a2fe448ef
pitr0theAct <120792390+pitr0theAct@users.noreply.github.com>
2022-12-29 09:40:49 +0300

    Add files via upload

---
+++ FirstProject/FirstProject.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include "Ball.hpp"
+#include "Animation.hpp"
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    ma::Animation Animation;
+    Animation.SetCaption("IDK");
+    Animation.SetResolution(500, 200);
+    Animation.Setup();
+    Animation.Run();
+}
+
+
+
