7160b789845b2e3333cace1ce07d1c12e707bd42
pitr0theAct <120792390+pitr0theAct@users.noreply.github.com>
2022-12-29 03:53:52 +0300

    Add files via upload

---
+++ Game/Game.cpp
@@ -0,0 +1,15 @@
+﻿
+#include "Func.hpp"
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    ma::Animation Animation;
+    Animation.SetCaption("IDK");
+    Animation.SetResolution(500, 200);
+    Animation.Setup();
+    Animation.Run();
+} 
+
+
+ 
