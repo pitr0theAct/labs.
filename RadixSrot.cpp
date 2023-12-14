045d2b6f4e299cf49a23e70973e8860040b6738d
pitr0theAct <120792390+pitr0theAct@users.noreply.github.com>
2023-12-15 02:47:20 +0300

    Add files via upload

---
+++ RadixSrot.cpp
@@ -0,0 +1,109 @@
+﻿#include <iostream>
+#include <vector>
+
+// Функция для нахождения максимального элемента
+int findMax(int arr[], int n) 
+{
+    int maxElement = arr[0];
+    for (int i = 1; i < n; i++) 
+    {
+        if (arr[i] > maxElement) 
+        {
+            maxElement = arr[i];
+        }
+    }
+    return maxElement;
+}
+
+
+
+// Функция для сортировки массива по указанному разряду
+void countSort(int arr[], int n, int exp) 
+{
+    const int RANGE = 10; // Диапазон чисел (0-9)
+    int* output = new int[n];// Вспомогательный массив для сохранения отсортированного массива(Динамическое выделение памяти)
+    int count[RANGE] = { 0 };// Массив для хранения количества элементов с определенными разрядами
+
+    // Подсчитываем количество элементов с определенными разрядами
+    for (int i = 0; i < n; i++) 
+    {
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    // Преобразуем count[i] так, чтобы count[i] содержал фактическую позицию
+    // данного разряда в отсортированном массиве output[]
+    for (int i = 1; i < RANGE; i++) 
+    {
+        count[i] += count[i - 1];
+        
+    }
+
+    // Строим отсортированный массив
+    for (int i = n - 1; i >= 0; i--) 
+    {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    // Копируем отсортированный массив output в исходный массив arr
+    for (int i = 0; i < n; i++) 
+    {
+        arr[i] = output[i];
+    }
+
+    delete[] output;//Отчистка памяти 
+}
+
+
+// Функция для сортировки массива методом корневой сортировки
+void radixSort(int arr[], int n) 
+{
+    // Находим максимальное число для определения количества разрядов
+    int maxElement = findMax(arr, n);
+
+    // Сортировка массива по каждому разряду
+    for (int exp = 1; maxElement / exp > 0; exp *= 10) 
+    {
+        countSort(arr, n, exp);
+    }
+}
+
+
+
+
+
+
+
+
+
+// Пример использования
+int main() 
+{
+    setlocale(LC_ALL, "Russian");
+    
+     
+
+
+
+    int const n = 8;
+    int mas[n] = { 170, 45, 75, 90, 802, 24, 2, 66 };
+
+    std::cout << "Исходный массив: ";
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << mas[i] << " ";
+    }
+
+    radixSort(mas, n);
+
+    std::cout << std::endl << std::endl;;
+    std::cout << "Отсортированный массив: ";
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << mas[i] << " ";
+    }
+
+    std::cout << std::endl;
+
+    
+}
---
+++ okongod.cpp
@@ -0,0 +1,223 @@
+﻿#include <iostream>
+#include<vector>
+#include<algorithm>
+
+
+
+//Функция для проверки скобок 
+bool Check(std::string s)
+{
+    //Создаем переменную i со значаением -1
+    int i = -1;
+
+    //Проходим по все строке
+    for (auto c : s) 
+    {
+        //Если находим открытую скобку то увеличиваем счетчик на 1 и заменяем i символ строки открытой скобкой
+        if (c == '(' || c == '{' || c == '[')
+        {
+
+            s[++i] = c;
+        }
+
+        else 
+        {
+            //Если это закрывающая скобка той же соответствующей открывающей скобки то уменьшаем i на -1
+            if (i >= 0 && ((s[i] == '(' && c == ')') || (s[i] == '{' && c == '}') || (s[i] == '[' && c == ']')))
+            {
+
+                i--;
+            }
+            //В противном случае false 
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+    if (i == -1)
+    {
+        return true;
+    }
+
+}
+
+
+//Функция для проверки деления на 0
+bool isValid(const std::string & expression) 
+{
+    for (auto i = 0; i < expression.length(); ++i) 
+    {
+        if (expression[i] == '/') 
+        {
+            // Проверяем, что все числа, после знака "/" являются ненулевыми
+            if (expression[i + 1] == '0') 
+            {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+
+//Функция для проверки, является ли символ оператором
+bool isOperator(char op) 
+{
+    return op == '+' || op == '-' || op == '*' || op == '/';
+}
+
+// Функция, которая вычисляет операцию над двумя числами
+double performOperation(double num1, double num2, char op) 
+{
+    if (op == '+') 
+    {
+        return num1 + num2;
+    }
+    else if (op == '-') 
+    {
+        return num1 - num2;
+    }
+    else if (op == '*') 
+    {
+        return num1 * num2;
+    }
+    else if (op == '/') 
+    {
+        return num1 / num2;
+    }
+    
+}
+
+// Функция для вычисления значения выражения
+double evaluateExpression(std::string expression) 
+{
+    std::vector<double> numbers;
+    std::vector<char> operators;
+
+    for (int i = 0; i < expression.length(); i++) 
+    {
+        // Если текущий символ - число, преобразуем его в число и добавляем в вектор чисел
+        if (isdigit(expression[i])) 
+        {
+            double num = 0;
+            while (i < expression.length() && isdigit(expression[i])) 
+            {
+                // Считываем все цифры числа
+                num = num * 10 + (expression[i] - '0');
+                i++;
+            }
+            numbers.push_back(num);
+            i--;
+        }
+        // Если текущий символ - оператор, добавляем в вектор операторов
+        else if (isOperator(expression[i])) 
+        {
+            while (!operators.empty() && isOperator(operators.back())) 
+            {
+                // Если приоритет текущего оператора меньше или равен приоритету оператора на вершине,
+                // выполним операцию с двумя последними числами и оператором на вершине
+                if ((expression[i] == '*' || expression[i] == '/') &&
+                    (operators.back() == '+' || operators.back() == '-')) 
+                {
+                    break;
+                }
+                double num2 = numbers.back();
+                numbers.pop_back();
+
+                double num1 = numbers.back();
+                numbers.pop_back();
+
+                char op = operators.back();
+                operators.pop_back();
+
+                numbers.push_back(performOperation(num1, num2, op));
+            }
+            operators.push_back(expression[i]);
+        }
+        // Если текущий символ - открывающая скобка, добавляем в вектор операторов
+        else if ((expression[i] == '(') || (expression[i] == '{') || (expression[i] == '['))
+        {
+            operators.push_back(expression[i]);
+        }
+        // Если текущий символ - закрывающая скобка, выполняем операции до открывающей скобки
+        else if (expression[i] == ')' || (expression[i] == '}') || (expression[i] == ']'))
+        {
+            while ((!operators.empty()) && (operators.back() != '(') && (operators.back() != '{') && (operators.back() != '['))
+            {
+                double num2 = numbers.back();
+                numbers.pop_back();
+
+                double num1 = numbers.back();
+                numbers.pop_back();
+
+                char op = operators.back();
+                operators.pop_back();
+
+                numbers.push_back(performOperation(num1, num2, op));
+            }
+            if (!operators.empty()) 
+            {
+                operators.pop_back();
+            }
+        }
+    }
+
+    //Выполняем оставшиеся операции
+    while (!operators.empty()) 
+    {
+        double num2 = numbers.back();
+        numbers.pop_back();
+
+        double num1 = numbers.back();
+        numbers.pop_back();
+
+        char op = operators.back();
+        operators.pop_back();
+
+        numbers.push_back(performOperation(num1, num2, op));
+    }
+
+   
+    return numbers.back();
+    
+}
+
+
+
+
+int main() 
+{
+    
+    setlocale(LC_ALL, "Russian");
+    
+    std::string expression;
+    std::cout << "Введите математическое выражение: ";
+    std::cin >> expression;
+
+    if (!(Check(expression)) && isValid(expression))
+    {
+        double result = evaluateExpression(expression);
+        std::cout << "Результат: " << result << std::endl;
+    }
+    else
+    {
+        std::cout << "Ошибка" << std::endl;
+    }
+
+    
+    
+    
+    
+}
+
+
+
+
+
+	
+
+
+
+
